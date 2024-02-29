b1564f5376b22c25ef8231a1d85df394dd8563c7
Pedro Figueroa <figueroash@gmail.com>
2024-02-29 23:01:55 +0100

    Add some jsdocs

--- src/components/Characters/Multiple/MultipleCharacters.tsx
+++ src/components/Characters/Multiple/MultipleCharacters.tsx
@@ -2,6 +2,16 @@ import React from 'react';
 import styles from './MultipleCharacters.module.scss';
 import SingleCharacter from '../Single/SingleCharacter.tsx';
 
+/**
+ * Interface for character objects.
+ * 
+ * @typedef {Object} Character
+ * @property {number} id - ID of the character.
+ * @property {string} name - Name of the character.
+ * @property {string | undefined} [description] - Description of the character (optional).
+ * @property {{ url: string }[]} urls - URLs related to the character.
+ * @property {{ path: string; extension: string }} thumbnail - Thumbnail details of the character.
+ */
 interface Character {
   id: number;
   name: string;
@@ -14,6 +24,14 @@ interface CharactersProps {
   characters: Character[];
 }
 
+/**
+ * Characters component.
+ * Displays multiple character items.
+ * 
+ * @param {Object} props - Props for the Characters component.
+ * @param {Character[]} props.characters - Array of character objects.
+ * @returns {JSX.Element | null} Characters component
+ */
 const Characters: React.FC<CharactersProps> = ({ characters }) => {
   return (
     characters && (
